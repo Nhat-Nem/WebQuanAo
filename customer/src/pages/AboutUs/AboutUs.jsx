1745c18a1fe293e661ecd9aec9b3f73a8bf6dcf6
Nhat-Nem <bnam64305@gmail.com>
2026-03-06 20:50:49 +0700

    Add files via upload

---
+++ customer/src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,13 @@
+import './AboutUs.css'
+
+function AboutUs() {
+    return (
+        <>  
+            <div className="about-page">
+                <h1>Bài này của nhóm: Lợi, Nam, Kiệt</h1>
+            </div>
+        </>
+    )
+}
+
+export default AboutUs
