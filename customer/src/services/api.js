a079b2cb21253a5980699082b6e44f235c6113f1
Nhat-Nem <bnam64305@gmail.com>
2026-03-27 19:53:16 +0700

    refactor api.js include env value

--- customer/src/services/api.js
+++ customer/src/services/api.js
@@ -1,19 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5050/api",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
 api.interceptors.response.use(
     res => res,
     err => {
-
         if (err.response?.status === 401 && 
             !err.config.url.includes("/auth/me")) {
             console.log("Not authenticated")
         }
-
         return Promise.reject(err)
     }
 )
