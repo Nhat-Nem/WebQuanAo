0011cf58942b29d3927bf925c50eda260fd4e34f
Nhat-Nem <bnam64305@gmail.com>
2026-03-31 16:28:01 +0700

    fix code

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -16,21 +16,3 @@ module.exports = function (req, res, next) {
         return res.status(401).json({message: "Token không hợp lệ"})
     }
 }
-
-module.exports = (req, res, next) => {
-    const authHeader = req.headers.authorization
-
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "Chưa đăng nhập" })
-    }
-
-    const token = authHeader.split(" ")[1]
-
-    try {
-        const decoded = jwt.verify(token, process.env.jwt)
-        req.user = decoded
-        next()
-    } catch (err) {
-        return res.status(401).json({ message: "Token không hợp lệ" })
-    }
-}
