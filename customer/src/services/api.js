ed8963bc6eee4175d511147453c417c1a9e09505
Nhat-Nem <bnam64305@gmail.com>
2026-04-03 10:51:48 +0700

    Remove request interceptor from API service
    Removed request interceptor for authorization token.

--- customer/src/services/api.js
+++ customer/src/services/api.js
@@ -4,15 +4,6 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_SERVER_API,
     withCredentials: true
 })
-
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem("token"); // lấy từ localStorage
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
 api.interceptors.response.use(
     res => res,
     err => {
